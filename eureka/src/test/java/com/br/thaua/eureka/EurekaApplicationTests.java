1420c45a8ff4f1197435ed6aa18ab119600619bc
Thauã Gabriel Sousa e Silva <tagashi.dev@gmail.com>
2025-06-21 18:24:57 -0300

    feat: adicionar estrutura

---
+++ eureka/src/test/java/com/br/thaua/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.thaua.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ reservation-service/src/test/java/com/br/thaua/reservation_service/ReservationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.thaua.reservation_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
