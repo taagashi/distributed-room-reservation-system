95efbbb32b3bea5c81fd0b8f536a0910c86e9517
Thauã Gabriel Sousa e Silva <tagashi.dev@gmail.com>
2025-06-25 13:54:22 -0300

    refactor: remanejar RabbitConfig para camada de menssageria

--- employee-service/src/main/java/com/br/thaua/employee_service/messaging/config/RabbitConfig.java
+++ employee-service/src/main/java/com/br/thaua/employee_service/messaging/config/RabbitConfig.java
@@ -1,4 +1,4 @@
-package com.br.thaua.employee_service.configuration;
+package com.br.thaua.employee_service.messaging.config;
 
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
