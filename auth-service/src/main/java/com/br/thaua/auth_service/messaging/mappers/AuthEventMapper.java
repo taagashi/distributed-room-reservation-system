ded589e491ff9774ee368ab422dfa871d4461e23
Thauã Gabriel Sousa e Silva <tagashi.dev@gmail.com>
2025-07-04 12:04:05 -0300

    refactor: diminuir complexidade do mapper

--- auth-service/src/main/java/com/br/thaua/auth_service/messaging/mappers/AuthEventMapper.java
+++ auth-service/src/main/java/com/br/thaua/auth_service/messaging/mappers/AuthEventMapper.java
@@ -1,22 +1,10 @@
 package com.br.thaua.auth_service.messaging.mappers;
 
 import com.br.thaua.auth_service.domain.Auth;
-import com.br.thaua.auth_service.domain.Role;
 import com.br.thaua.auth_service.messaging.dto.AuthEvent;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-
-import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface AuthEventMapper {
-    @Mapping(target = "roles", expression = "java(toEventRole(auth.getRoles()))")
     AuthEvent map(Auth auth);
-
-    default List<String> toEventRole(List<Role> roles) {
-        if(roles == null) {
-            return  null;
-        }
-        return roles.stream().map(Enum::name).toList();
-    }
 }
