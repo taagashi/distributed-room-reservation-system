59f0b66a4bafcaffcee24ee94501a45d62fdcd13
Thauã Gabriel Sousa e Silva <tagashi.dev@gmail.com>
2025-08-24 20:52:12 -0300

    feat: adicionar camada de persistencia

--- reservation-service/src/main/java/com/br/thaua/reservation_service/persistence/ParticipantRepository.java
+++ reservation-service/src/main/java/com/br/thaua/reservation_service/persistence/ParticipantRepository.java
@@ -6,18 +6,65 @@ import com.br.thaua.reservation_service.persistence.models.ParticipantEntity;
 import com.br.thaua.reservation_service.persistence.models.ReservationEntity;
 import com.br.thaua.reservation_service.persistence.models.TypeParticipantEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface ParticipantRepository extends JpaRepository<ParticipantEntity, Long> {
+    @Query("""
+            SELECT p FROM ParticipantEntity p
+            WHERE p.authId = ?1
+            AND p.typeParticipant = ?2
+            AND p.reservation = ?3
+            """)
     ParticipantEntity findByAuthIdAndTypeParticipantAndReservation(Long authId, TypeParticipantEntity typeParticipant, ReservationEntity reservationEntity);
+
+    @Query("""
+            SELECT p FROM ParticipantEntity p
+            WHERE p.authId = ?1
+            AND p.typeParticipant = ?2
+            AND p.reservation.id = ?3
+            """)
     ParticipantEntity findByAuthIdAndTypeParticipantAndReservationId(Long authId, TypeParticipantEntity typeParticipant, Long reservationId);
+
+    @Query("""
+            SELECT p FROM ParticipantEntity p
+            WHERE p.authId = ?1
+            """)
     List<ReservationEntity> findAllReservationByAuthId(Long authId);
+
+    @Query("""
+            SELECT p FROM ParticipantEntity p
+            WHERE p.reservation = ?1
+            """)
     List<ParticipantEntity> findAllByReservation(ReservationEntity reservationEntity);
+
+    @Query("""
+            SELECT p FROM ParticipantEntity p
+            WHERE p.typeParticipant = ?1
+            AND p.reservation.id = ?2
+            """)
     List<ParticipantEntity> findAllByTypeParticipantAndReservationId(TypeParticipantEntity map, Long reservationId);
+
+    @Query("""
+            SELECT p FROM ParticipantEntity p
+            WHERE p.cheackin = ?1
+            AND p.reservation.id = ?2
+            """)
     List<ParticipantEntity> findAllByCheackinAndReservationId(boolean cheackin, Long reservationId);
+
+    @Query("""
+            SELECT p FROM Participant p
+            WHERE p.reservation.id = ?1
+            """)
     List<ParticipantEntity> findAllByReservationId(Long reservationId);
+
+    @Query("""
+            SELECT p FROM ParticipantEntity p
+            WHERE p.authId = ?1
+            AND p.reservation.id = ?2
+            """)
     ParticipantEntity findByAuthIdAndReservationId(Long authId, Long reservationId);
 }
--- reservation-service/src/main/java/com/br/thaua/reservation_service/persistence/ReservationRepository.java
+++ reservation-service/src/main/java/com/br/thaua/reservation_service/persistence/ReservationRepository.java
@@ -2,6 +2,7 @@ package com.br.thaua.reservation_service.persistence;
 
 import com.br.thaua.reservation_service.persistence.models.ReservationEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
@@ -10,5 +11,12 @@ import java.util.List;
 
 @Repository
 public interface ReservationRepository extends JpaRepository<ReservationEntity, Long> {
+    @Query("""
+            SELECT re FROM ReservationEntity re
+            WHERE re.roomId = ?1
+            AND re.date = ?2
+            AND re.start <= ?3
+            AND re.end >= ?4
+            """)
     List<ReservationEntity> findAllByRoomIdAndDateAndStartLessThanEqualAndEndGreaterThanEqual(Long roomId, LocalDate date, LocalTime end, LocalTime start);
 }
