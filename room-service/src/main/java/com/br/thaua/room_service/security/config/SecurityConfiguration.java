d2911e25d48c692fd15bb96cd15f321e8af7262e
Thauã Gabriel Sousa e Silva <tagashi.dev@gmail.com>
2025-09-01 20:51:23 -0300

    feat: adicionar camada de seguranca

--- room-service/src/main/java/com/br/thaua/room_service/security/config/SecurityConfiguration.java
+++ room-service/src/main/java/com/br/thaua/room_service/security/config/SecurityConfiguration.java
@@ -1,20 +1,31 @@
 package com.br.thaua.room_service.security.config;
 
+import com.br.thaua.room_service.security.filters.SecurityFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.FormLoginConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+@Configuration
+@RequiredArgsConstructor
 public class SecurityConfiguration {
+    private final SecurityFilter securityFilter;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable);
-        http.httpBasic(HttpBasicConfigurer::disable);
-        http.headers(header -> header.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable));
-        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-        http.addFilterBefore()
+        return http.csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(HttpBasicConfigurer::disable)
+                .formLogin(FormLoginConfigurer::disable)
+                .headers(header -> header.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
     }
 }
--- room-service/src/main/java/com/br/thaua/room_service/security/filters/SecurityFilter.java
+++ room-service/src/main/java/com/br/thaua/room_service/security/filters/SecurityFilter.java
@@ -1,18 +1,46 @@
 package com.br.thaua.room_service.security.filters;
 
+import com.br.thaua.room_service.http.dto.GatewayRequest;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.List;
 
 @Service
 public class SecurityFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String signatureGateway =  request.getHeader("x-signature-gateway");
 
+//        if(signatureGateway == null) {
+//            response.sendError(401);
+//            filterChain.doFilter(request, response);
+//            return;
+//        }
+
+        String id = request.getHeader("x-user-id");
+        String name = request.getHeader("x-user-name");
+        String email = request.getHeader("x-user-email");
+        String roles = request.getHeader("x-user-roles");
+
+        if(id == null || name == null || email == null || roles == null) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        List<String> rolesList = List.of(roles.split(","));
+        GatewayRequest gatewayRequest = new GatewayRequest(Long.valueOf(id), name, email, rolesList);
+        Authentication authentication = new UsernamePasswordAuthenticationToken(gatewayRequest, null, gatewayRequest.roles().stream().map(SimpleGrantedAuthority::new).toList());
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        filterChain.doFilter(request, response);
     }
 }
