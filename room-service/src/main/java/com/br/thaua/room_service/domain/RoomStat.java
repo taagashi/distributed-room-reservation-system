a5fa6e5296102836f6ec903a23f9eea3915f628b
Thauã Gabriel Sousa e Silva <tagashi.dev@gmail.com>
2025-08-31 22:01:25 -0300

    feat: adicionar camada de domain

--- room-service/src/main/java/com/br/thaua/room_service/domain/RoomStat.java
+++ room-service/src/main/java/com/br/thaua/room_service/domain/RoomStat.java
@@ -11,17 +11,7 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 @Getter
 public enum RoomStat {
-    AVAILABLE(1L, "Available"),
-    BUSY(2L, "Busy"),
-    MAINTENANCE(3L, "Maintenance");
-
-    private final Long id;
-    private final String stat;
-
-    private final static Map<Long, RoomStat> BY_ID = Arrays.stream(values())
-            .collect(Collectors.toUnmodifiableMap(RoomStat::getId, Function.identity()));
-
-    public static RoomStat fromId(Long roomStatId) {
-        return BY_ID.get(roomStatId);
-    }
+    AVAILABLE,
+    BUSY,
+    MAINTENANCE
 }
